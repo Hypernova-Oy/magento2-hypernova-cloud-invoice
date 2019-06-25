e59db1b0350eb66326e4104cca77efdb7d842216
Lari Taskula <lari.taskula@hypernova.fi>
2019-06-25 23:00:58 +0300

    Add option to set invoice language

--- src/Observer/SavePdfInvoice.php
+++ src/Observer/SavePdfInvoice.php
@@ -9,17 +9,22 @@ class SavePdfInvoice implements ObserverInterface
     private $dir;
     private $pdfInvoiceModel;
     private $scopeConfig;
+    private $localeInterface;
+
+    private $invoiceLocale;
 
     private $googleDrive = 0;
 
     public function __construct(
         \Magento\Framework\Filesystem\DirectoryList $dir,
         \Magento\Sales\Model\Order\Pdf\Invoice $pdfInvoiceModel,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\TranslateInterface $localeInterface
     ) {
         $this->dir = $dir;
         $this->pdfInvoiceModel = $pdfInvoiceModel;
         $this->scopeConfig = $scopeConfig;
+        $this->localeInterface = $localeInterface;
 
         if ($this->scopeConfig->getValue(
             'cloud_invoice/google_drive/enable_google_drive',
@@ -27,11 +32,22 @@ class SavePdfInvoice implements ObserverInterface
         ) == 1) {
             $this->googleDrive = 1;
         }
+
+        $this->currentLocale = $this->localeInterface->getLocale();
+        $this->invoiceLocale = $this->currentLocale;
+        if ($locale = $this->scopeConfig->getValue(
+            'cloud_invoice/general/invoice_language',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE)) {
+            $this->invoiceLocale = $locale;
+        }
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         try {
+            $this->localeInterface->setLocale($this->invoiceLocale);
+            $this->localeInterface->loadData();
+
             $invoice = $observer->getEvent()->getInvoice();
 
             if ($this->googleDrive) {
@@ -44,10 +60,15 @@ class SavePdfInvoice implements ObserverInterface
                 $invoice->addComment('Added to Google Drive', false, false);
             }
 
+            $this->localeInterface->setLocale($this->currentLocale);
+            $this->localeInterface->loadData();
         } catch (Exception $e) {
+            $this->localeInterface->setLocale($this->currentLocale);
+            $this->localeInterface->loadData();
             $invoice->addComment($e->getMessage(), false, false);
             $invoice->save();
         }
+
         return $this;
     }
 }
